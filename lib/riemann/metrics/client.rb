6fc60b6532d5c42b695777efe04c43168e4486b0
Erik Nygren <sten.erik.nygren@gmail.com>
2016-06-08 16:50:32 -0400

    added opentsdb style to initializor and a more concise reporting function

--- lib/riemann/metrics/client.rb
+++ lib/riemann/metrics/client.rb
@@ -11,13 +11,14 @@ module Riemann
 
       TTL = 10
 
-      def initialize host, port, service_name, riemann_env, ttl
+      def initialize host, port, service_name, riemann_env, ttl, opentsdb_style
         @host = host
         @port = port
         @service_name = service_name
         @ttl = ttl || TTL
         @riemann_env = riemann_env || 'none'
         @hostname = get_hostname
+        @opentsdb_style = opentsdb_style || false
       end
 
       def client
@@ -25,16 +26,23 @@ module Riemann
       end
 
       def gauge tags, state, metric, service='', description=nil
+        t = tags.is_a?(Hash) ? tags.map{|k,v|"#{k}=#{v}"} : tags
+        the_tags = !@opentsdb_style ? (t.dup << @riemann_env) : (t.dup << "env=#{riemann_env}")
         event = {
           host: @hostname,
           state: state,
           metric: metric,
           ttl: @ttl,
-          tags: (tags.dup << @riemann_env),
+          tags: the_tags,
           service: "#{@service_name}.#{service}"
         }
         event[:description] = description if description
         client << event
+      end
+
+      def report(service, metric, tags)
+
+        gauge(t, OK, metric, service)
       end
 
       def get_hostname
